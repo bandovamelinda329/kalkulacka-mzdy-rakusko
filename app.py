4360617c07ee113fd7990ada4ac1d07e62f9b688
Marek Banda <bandovamelinda329@gmail.com>
2026-09-09 11:52:52 +0200

    Pridany AdSense kod

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ HTML = """
 <!DOCTYPE html>
 <html lang="sk">
 <head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6595281967559794"
+     crossorigin="anonymous"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Kalkulačka mzdy Rakúsko</title>
